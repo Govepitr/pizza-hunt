5bdbfce77f78929286120dcfe3c8dfbb6aea76cb
Dan Arbelo <govepitr@gmail.com>
2021-04-19 11:21:38 -0700

    Updated Comment-controllers w/date format

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.use('/api', apiRoutes);
 router.use('/', htmlRoutes);
 
 router.use((req, res) => {
-  res.status(404).send('<h1>*😝* 404 Error!</h1>');
+  res.status(404).send('<h1>🖕🏾😝🖕🏾 404 Error!</h1>');
 });
 
 module.exports = router;
